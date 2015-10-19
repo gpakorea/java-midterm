280adeb2654c9baac119946036b983d47411a1a9
Aaron Snowberger <jekkilekki@gmail.com>
2015-10-19 15:41:50 +0900

    Simpler test option

---
+++ SimpleTest.java
@@ -0,0 +1,36 @@
+/**
+ * SimpleTest.java
+ * 
+ * Write your own program that:
+ * 1) Uses Scanner for input
+ * 2) Uses a for loop
+ * 3) Uses a while loop
+ * 4) Uses an IF / ELSE IF / ELSE
+ * 5) Uses a switch
+ * 6) Uses an array
+ */
+public class SimpleTest {
+  Scanner s;
+  
+  for () {
+    
+  } // end for
+  
+  while () {
+    
+  } // end while
+  
+  if () {
+    
+  } else if () {
+    
+  } else {
+    
+  } // end if/else
+  
+  switch() {
+    
+  } // end switch
+  
+  // array
+}
